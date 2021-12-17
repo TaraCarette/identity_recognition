4da94cf80c4f3cdccc0bb67a0f482a2244b82309
TaraCarette <terra.the.great@gmail.com>
2021-12-17 14:15:09 +0100

    The current choregraphe progress, quite rough

---
+++ hri_identity_recognition/translations/translation_en_US.ts
@@ -0,0 +1,51 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+    <context>
+        <name>behavior_1/behavior.xar:/Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Learning is fun</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Learning is fun</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say (1)</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <source>It's Tara</source>
+            <comment>Text</comment>
+            <translation type="obsolete">It's Tara</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Bummer</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Bummer</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say (2)</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Detected</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Detected</translation>
+        </message>
+    </context>
+</TS>
